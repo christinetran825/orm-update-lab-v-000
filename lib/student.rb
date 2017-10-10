ef3585d320db0f709915fa8ffeb535b674caba56
Christine Tran <christinetran825@gmail.com>
2017-10-10 05:11:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,18 +6,20 @@ class Student
   #  with DB[:conn]
 
   def initialize
-    
+
   end
 
   def self.create_table
-    
+
   end
 
   def self.drop_table
-    
+
   end
 
+  def save
 
+  end
 
 
 end
