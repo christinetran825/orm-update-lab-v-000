44674bb5dbbcc67b94a3cdeadf8ceaee47610339
Christine Tran <christinetran825@gmail.com>
2017-10-10 05:11:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,19 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def initialize
+    
+  end
+
+  def self.create_table
+    
+  end
+
+  def self.drop_table
+    
+  end
+
+
+
 
 end
