048641ce9ca99bc4cf5d274fc1c621f411c10c6f
Christine Tran <christinetran825@gmail.com>
2017-10-10 05:13:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,10 @@ class Student
 
   attr_accessor :id, :name, :grade
 
-  def initialize
-
+  def initialize(id:nil, name:, grade:)
+    @id = id
+    @name = name
+    @grade = grade
   end
 
   def self.create_table
